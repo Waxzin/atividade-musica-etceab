e1ee7adacf40a5bc2c43397e485b34f7db5b7c5f
Matheus luan <matheusluan042gmail.com>
2026-03-17 22:34:37 -0300

    Conclução da atividade de Musica

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("The Real Slim Shady");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("I'm Slim Shady, yes, I'm the real Shady");
+Console.WriteLine("All you other Slim Shadys are just imitatin");
+Console.WriteLine("So won't the real Slim Shady, please, stand up?");
+Console.WriteLine("Please, stand up");
+Console.WriteLine("Please, stand up");
+Console.WriteLine("I'm Slim Shady, yes, I'm the real Shady");
+Console.WriteLine("All you other Slim Shadys are just imitatin");
+Console.WriteLine("So won't the real Slim Shady, please, stand up?");
+Console.WriteLine("Please, stand up");
+Console.WriteLine("Please, stand up");
+
